e5c4ba705ae9e4025c97c75a2bf072ebae9f6862
VICIWUOHA <viciwuoha@gmail.com>
2023-11-12 16:49:13 +0100

    initial onboarding

---
+++ archiver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# this file should be runnable using the command * bash archiver.sh *
+echo "Hello EDIKAN archival in progress...." 
+echo "anything you want again"
+mv folder/to/move destination/archive
+echo "any message you want after success."
+# replace the “folder/to/move” and “destination/folder” with the correct info as shown above.
+# (optional) add a line below to list all the files in the destination/folder using the ls command
